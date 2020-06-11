8cc5d1d6a31faa5174345083782a4f743d83163a
Deepan Reguraman <Deepan.Reguraman@centurylink.com>
2020-06-11 16:52:50 +0530

    zuul

---
+++ zuul-api-gateway-server/src/main/java/com/spring/micro/service/zuulapigatewayserver/filter/ZuulErrorFilter.java
@@ -0,0 +1,34 @@
+package com.spring.micro.service.zuulapigatewayserver.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class ZuulErrorFilter extends ZuulFilter {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		Throwable throwable = RequestContext.getCurrentContext().getThrowable();
+		logger.info("Error Response: {}", throwable.getMessage());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "post";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+}
